cce8546516a37bd8263faba1ab9d618b333e52b3
chponya <chponyaaa@gmail.com>
2025-03-02 02:45:47 +0200

    Add: Добавлен шаблон приложения
    Видео демонстрация прилагается

--- hw3/plant_care_base/android/app/src/main/kotlin/com/example/plant_care_base/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.plant_care_base
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- hw3/plant_care_base/lib/models/plant_model.dart
+++
@@ -1,7 +0,0 @@
-class Plant {
-  final String name;
-  final String description;
-  final String imageUrl;
-
-  Plant({required this.name, required this.description, required this.imageUrl});
-}
